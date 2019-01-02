252cda292c4bb73aded80330198bc9b865695070
Jamie <wenkangjing@gmail.com>
2019-01-02 23:34:56 +1100

    Finish types of handbook and deep dive

---
+++ ts/sandbox/types.ts
@@ -0,0 +1,14 @@
+let myBool: boolean = false;
+myBool = true;
+console.log('myBool: ', myBool);
+
+let myTuple: [string, number];
+myTuple = ['a', 1];
+console.log('myTuple: ', myTuple);
+
+enum Color { Red = 1, Green, Blue }
+let myColor: Color = Color.Green;
+console.log('myColor: ', myColor);
+
+let list: any[] = [1, true, "free"];
+console.log('list: ', list);
